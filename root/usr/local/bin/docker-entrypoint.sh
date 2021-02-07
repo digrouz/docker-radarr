2d3343d0afe611b95943a06c9f155959423905ef
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-02-07 11:31:57 +0100

    added support .net core version

--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -10,7 +10,7 @@ MYGID="${APPGID}"
 ConfigureUser
 AutoUpgrade
 
-if [ "$1" = 'radarr' ]; then
+if [ "$1" = 'radarr-mono' ]; then
   if [ -d  /config ]; then
     chown -R "${MYUSER}":"${MYUSER}" /config
   fi
@@ -19,6 +19,14 @@ if [ "$1" = 'radarr' ]; then
   
   DockLog "Starting app: ${@}"
   exec su-exec "${MYUSER}" mono --debug /opt/Radarr/Radarr.exe -no-browser -data=/config
+elif [ "$1" = 'radarr-core' ]; then
+      chown -R "${MYUSER}":"${MYUSER}" /config
+  fi
+
+  RunDropletEntrypoint
+
+  DockLog "Starting app: ${@}"
+  exec su-exec "${MYUSER}" /opt/Radarr/Radarr -no-browser -data=/config
 else
   DockLog "Starting command: ${@}"
   exec "$@"
