eb0ff2795c0c511b2dfdca13ce4f6f102da71dfe
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-16 20:00:36 +0200

    cut starting v of version

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,8 +2,8 @@
 
 RADARR_URL="https://api.github.com/repos/Radarr/Radarr/tags"
 
-
-LAST_VERSION=$(curl -SsL ${RADARR_URL} | jq .[0].name -r )
+FULL_LAST_VERSION=$(curl -SsL ${RADARR_URL} | jq .[0].name -r )
+LAST_VERSION="${FULL_LAST_VERSION:1}"
 
 sed -i -e "s|RADARR_VERSION='.*'|RADARR_VERSION='${LAST_VERSION}'|" Dockerfile_*
 
