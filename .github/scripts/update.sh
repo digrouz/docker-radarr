5f123371c8e32259a7519b403cf7f8ecc3fdbdbf
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-04-16 19:47:12 +0200

    added github actions

---
+++ .github/scripts/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SABNZBD_URL="https://api.github.com/repos/Radarr/Radarr/tags"
+
+
+LAST_VERSION=$(curl -SsL ${RADARR_URL} | jq .[0].name -r )
+
+sed -i -e "s|RADARR_VERSION='.*'|RADARR_VERSION='${LAST_VERSION}'|" Dockerfile_*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
