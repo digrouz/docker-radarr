7db564bd86784697711ac3214f7cb4f88d031856
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-19 13:37:59 +0200

    Update update.sh

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -5,7 +5,7 @@ RADARR_URL="https://api.github.com/repos/Radarr/Radarr/tags"
 FULL_LAST_VERSION=$(curl -SsL ${RADARR_URL} | jq .[0].name -r )
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
-sed -i -e "s|RADARR_VERSION='.*'|RADARR_VERSION='${LAST_VERSION}'|" Dockerfile_*
+sed -i -e "s|RADARR_VERSION='.*'|RADARR_VERSION='${LAST_VERSION}'|" Dockerfile*
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
