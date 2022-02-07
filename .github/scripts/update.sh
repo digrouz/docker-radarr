8cb8686c3199ffbe7682b0a877388772d638b116
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2022-02-07 07:32:54 +0000

    updated url

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RADARR_URL="https://radarr.servarr.com/v1/update/master/changes?os=linux"
+RADARR_URL="https://radarr.servarr.com/v1/update/master/changes?runtime=netcore&os=linux"
 
 LAST_VERSION=$(curl -SsL ${RADARR_URL} | jq .[0].version -r )
 
